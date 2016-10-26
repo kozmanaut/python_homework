4c8637e41e86991e6223188365bed6f0f2f27e68
kozmanaut <radokozma@gmail.com>
2016-10-26 13:00:28 +0200

    Updated raxml project

--- project/README.txt
+++ project/README.txt
@@ -4,19 +4,32 @@ ANGSD Genotype to Raxml parser
 
 name: raxml_parser
 
-What will it do: Take a genotype file created from ANGSD, clean it (remove any line that has NN), format it into a fasta file that will be used as input for Raxml.
-Run Raxml as well? 
-Include additional Raxml options?
+What will it do: Take a genotype file created from ANGSD, clean it (remove any line that has NN), format it into a fasta file that will be used as input for Raxml.Use a Raxml wrapper to run Raxml locally. 
 
 For names, have another input file with ind names
 
 Input: 	ANGSD genotype file
 Output: Phylo tree created from the Raxml .tre file
 
-Class: SNP/FASTA
 
-attributes: name, length (bp's), number of ind/seq's
-methods: parse genotype file and name file into combined Fasta file
+Class Genotype()
+	SNP.single_2b_genotypes
+		Take each genotype (AA) of each individual and store it
+	SNP.create_1b_genotypes()
+		a method that will convert the genotype into IUPAC nt code using a dictionary
+Class Alignment()
+	Alignemnt.sequences
+		A sequence of each individuals
+	Alignment.add_SNP
+		a method that uses an object of class SNP to poopulate a sequences with one more position
+	Alignment.clean_N
+		method to remove any position across all individuals that has N
+	alignment.indnames()
+		method that uses an input file to create names for fasta file
+	Alingment_check_same_number_bases
+		method that checks that each individuals has the same number of bases
+	alignment.to_fasta("filepath")
+		method that creates a fasta file with all the individuals
 
 
 Class: Raxml_Runner 
@@ -30,3 +43,6 @@ Class: Tree
 	- use the Phylo module from Biopython
 attributes: self, path to .tre file
 methods: draw tree, (convert between formats?)
+
+
+
--- project/raxml_parser.py
+++ project/raxml_parser.py
@@ -1,17 +1,52 @@
 """
 A parser for Raxml. The input is a genotype file produced in ANGSD, that has the genotypes of all individuals present on one line,
 separated by a tab. It will then create a fasta file, containing the concatenated sequence for each individual, across all individuals
-Raxml will then be run on the fasta file, using the Raxml wrapper in Biopython
+Raxml will then be run on the fasta file, using the Raxml wrapper in Biopython.
 """
 
 import os
 import sh
+import matplotlib
+import matplotlib.pyplot as plt
 try:
 	from Bio import Phylo
 	from Bio.Phylo.Applications import RaxmlCommandline
 except ImportError:
 	raise "Biopython is required but it does not seem to be installed. Please install it."
 
+####################################################################
+class Genotype(object):
+	"""Stores the genotypes of each individual at each position. Path to the genotype file is the only input"""
 
-class Snps(self, path):
-	""" """
+	def __init__(self, path):
+		self.path = path
+		if not os.path.exists(self.path):
+			raise IOError("File does not exist")
+
+		# Store the genotypes in a list of tuples and calculate the number of individuals in the file
+		rows = []
+		with open(self.path, 'r') as data:
+			for line in data:
+				line = line.strip('\n').split('\t')
+				rows.append(line[4:-1])
+				num_ind =  len(line) - 5
+
+		geno = zip(*rows)
+		self.genotypes = geno
+		self.num_ind = num_ind
+
+	def single_base(self):
+		nc_dict = {
+			"AA" : "A", "CC": "C", "TT": "T", "GG": "G"
+			"AC": "M", "CA": "M", "AT": "W", "TA": "W", "AG": "R", "GA": "R"
+			"CT": "Y", "TC": "Y", "CG": "S", "GC": "S"
+			"GT": "K", "TG": "K"
+			"NN": "N"
+			}
+
+
+
+###################################################################
+test = Genotype("genotypes.subset.txt")
+print test.num_ind
+print test.genotypes
